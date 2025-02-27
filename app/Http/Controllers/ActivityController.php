c8d5d337f52726a064c25e971178df5dce2132f0
Mochamad Idris <idrez.mochamad@gmail.com>
2025-02-27 07:11:29 +0000

    fix report stase 1

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -706,10 +706,8 @@ class ActivityController extends Controller
         // dd($userStaseCounts);
 
         $stases = Stase::whereHas('units', function ($query) use ($user, $unitSelected) {
-            if ($user->roles->count() == 1) {
-                if ($user->hasRole('student')) {
-                    $query->where('units.id', $user->student_unit_id);
-                }
+            if ($user->hasRole('student')) {
+                $query->where('units.id', $user->student_unit_id);
             }else{
                 $query->where('units.id', $unitSelected);
             }
